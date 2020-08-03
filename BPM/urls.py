6c102ede1b6a0d44a1c9084fa0ddb8713f894e4d
sarahlevins <sarahjeanlevins@gmail.com>
2020-08-03 19:26:33 +0800

    base app working with users

--- BPM/urls.py
+++ BPM/urls.py
@@ -19,4 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('bpmsearchapp.urls')),
     path('admin/', admin.site.urls),
+    path('users/', include('django.contrib.auth.urls')),
+    path('users/', include('users.urls'))
+
 ]
--- bpmsearchapp/modules/spotify.py
+++ bpmsearchapp/modules/spotify.py
@@ -1,5 +1,11 @@
 import os
-import base64, json, requests, datetime
+import base64
+import json
+import requests
+import datetime
+import pprint
+from datetime import timedelta
+
 
 def get_access_token():
     CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
@@ -13,6 +19,7 @@ def get_access_token():
     header = {"Authorization": "Basic {}".format(b64_auth_str)}
 
     return requests.post(url, data=grant_type, headers=header).json()["access_token"]
+
 
 def pretty_time_delta(seconds):
     sign_string = '-' if seconds < 0 else ''
@@ -32,54 +39,130 @@ def pretty_time_delta(seconds):
 
 def get_song_detail(song_title):
     access_token = get_access_token()
-    url = f"https://api.spotify.com/v1/search?q={song_title}&type=track"
     header_list = {
         "Accept": "application/json",
         "Content-Type": "application/json",
         "Authorization": f"Bearer {access_token}",
     }
-    response = requests.get(url, headers=header_list).json()["tracks"]["items"][0]
-    print(response)
+    track_detail_response = requests.get(
+        f"https://api.spotify.com/v1/search?q={song_title}&type=track", headers=header_list).json()["tracks"]["items"][0]
+    audio_features_response = requests.get(
+        f"https://api.spotify.com/v1/audio-features?ids={track_detail_response['id']}", headers=header_list).json()['audio_features'][0]
     song_detail = {
-        'song_title': response['name'],
-        'id': response['id'],
-        'duration_ms': response['duration_ms'],
-        'artist': response['artists'][0]['name'],
-        'duration': pretty_time_delta(int(response['duration_ms']/1000))
+        'song_title': track_detail_response['name'],
+        'id': track_detail_response['id'],
+        'duration_ms': track_detail_response['duration_ms'],
+        'artist': track_detail_response['artists'][0]['name'],
+        'duration': pretty_time_delta(int(track_detail_response['duration_ms']/1000)),
+        'tempo': audio_features_response['tempo']
     }
     return song_detail
 
-def get_audio_features(song_id):
-    access_token = get_access_token()
-    url = f"https://api.spotify.com/v1/audio-features?ids={song_id}"
-    header_list = {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {access_token}",
-    }
-    return requests.get(url, headers=header_list).json()['audio_features'][0]
 
-def get_song_recommendations(song_id, tempo):
+def get_song_recommendations(song_id, tempo, duration):
     access_token = get_access_token()
-    url = f"https://api.spotify.com/v1/recommendations?market=AU&limit=10&seed_tracks={song_id}&target_tempo={tempo}"
     header_list = {
         "Accept": "application/json",
         "Content-Type": "application/json",
         "Authorization": f"Bearer {access_token}",
     }
     recommendations = []
-    response = requests.get(url, headers=header_list).json()['tracks']
-
-    for i in range(10):
-        audio_features=get_audio_features(response[i]['id'])
+    extra_recommendations = []
+    recc_response = requests.get(
+        f"https://api.spotify.com/v1/recommendations?market=AU&limit=90&seed_tracks={song_id}&target_tempo={tempo}", headers=header_list).json()['tracks']
+    response_ids = ','.join([track['id'] for track in recc_response])
+    feat_response = requests.get(
+        f"https://api.spotify.com/v1/audio-features/?ids={response_ids}", headers=header_list).json()['audio_features']
+    i = 0
+    target_duration = datetime.timedelta(minutes=duration+3)
+    duration = datetime.timedelta(minutes=0)
+    while duration < target_duration:
         recommendations.append(
             {
-            'artist' : response[i]['artists'][0]['name'],
-            'song_title' : response[i]['name'],
-            'duration_ms' : response[i]['duration_ms'],
-            'tempo' : audio_features['tempo'],
-            'id' : response[i]['id'],
-            'duration': pretty_time_delta(int(response[i]['duration_ms']/1000))
+                'artist': recc_response[i]['artists'][0]['name'],
+                'song_title': recc_response[i]['name'],
+                'duration_ms': recc_response[i]['duration_ms'],
+                'id': recc_response[i]['id'],
+                'tempo': feat_response[i]['tempo'],
+                'duration': pretty_time_delta(int(recc_response[i]['duration_ms']/1000))
             }
         )
+        duration += datetime.timedelta(
+            milliseconds=recc_response[i]['duration_ms'])
+        i += 1
+    while i < 89:
+        extra_recommendations.append(
+            {
+                'artist': recc_response[i]['artists'][0]['name'],
+                'song_title': recc_response[i]['name'],
+                'duration_ms': recc_response[i]['duration_ms'],
+                'id': recc_response[i]['id'],
+                'tempo': feat_response[i]['tempo'],
+                'duration': pretty_time_delta(int(recc_response[i]['duration_ms']/1000))
+            }
+        )
+        i += 1
     return recommendations
+
+
+class Track():
+    def __init__(self, title):
+        self.title = title
+
+    def get_song_detail(self):
+        access_token = get_access_token()
+        header_list = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {access_token}",
+        }
+        track_detail_response = requests.get(
+            f"https://api.spotify.com/v1/search?q={self.title}&type=track", headers=header_list).json()["tracks"]["items"][0]
+        audio_features_response = requests.get(
+            f"https://api.spotify.com/v1/audio-features?ids={track_detail_response['id']}", headers=header_list).json()['audio_features'][0]
+
+        self.id = track_detail_response['id']
+        self.duration_ms = track_detail_response['duration_ms']
+        self.artist = track_detail_response['artists'][0]['name']
+        self.duration = pretty_time_delta(
+            int(track_detail_response['duration_ms']/1000))
+        self.tempo = audio_features_response['tempo']
+        return self
+
+    def get_song_recommendations(self, tempo):
+        access_token = get_access_token()
+        header_list = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {access_token}",
+        }
+        self.recommendations = []
+        recc_response = requests.get(
+            f"https://api.spotify.com/v1/recommendations?market=AU&limit=90&seed_tracks={self.id}&target_tempo={tempo}", headers=header_list).json()['tracks']
+        feat_response = requests.get(
+            f"https://api.spotify.com/v1/audio-features/?ids={','.join([track['id'] for track in recc_response])}", headers=header_list).json()['audio_features']
+        for i in enumerate(recc_response):
+            i = i[0]
+            track = Track(recc_response[i]['name'])
+            track.artist = recc_response[i]['artists'][0]['name']
+            track.duration_ms = recc_response[i]['duration_ms']
+            track.id = recc_response[i]['id']
+            track.tempo = feat_response[i]['tempo']
+            track.duration = pretty_time_delta(
+                int(recc_response[i]['duration_ms']/1000))
+            self.recommendations.append(track)
+        return self
+
+    def create_duration_playlist(self, duration):
+        if not self.recommendations:
+            self.get_song_recommendations(self, 150)
+        else:
+            self.duration_playlist = [self]
+            target_duration = datetime.timedelta(minutes=duration+3)
+            duration = datetime.timedelta(minutes=0)
+            for i in enumerate(self.recommendations):
+                while duration < target_duration:
+                    self.duration_playlist.append(self.recommendations[i[0]])
+                    duration += datetime.timedelta(
+                        milliseconds=self.recommendations[i[0]].duration_ms)
+        return self
